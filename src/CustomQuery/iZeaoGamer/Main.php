9aee59bce1dee553e3c7894d27899ea216f78d2b
iZeaoGamer <jameshayward14@icloud.com>
2019-05-01 11:04:19 +0100

    !== NEW UPDATE AVAILABLE v1.0.2-ALPHA !==
    Finally added commands to CustomQuery!
    Added /customquery.
    Added /customquery reload - Reload your configurations file without the need of restarting the server!
    Added /customquery <settings> <value>
    Added permission for /customquery.
    Added /customquery list-plugins <value>
    Added /customquery infinity-slots <value>
    Added /customquery enable-serverlist-motd <value>
    Added /customquery fake-slots <value>
    Added /customquery default-server-name <value>
    Added /customquery set-plugins <plugins>
    Added /customquery motd-serverlist-message <message>
    Added /customquery new-player-motd <message>
    Added /customquery old-player-motd <message>
    Added /customquery min-slots <number>
    Added /customquery max-slots <number>
    Add some to-dos to README.MD
    Bump plugin version to v1.0.3 (Not ready for release/PRE release yet.)
    
    Have any issues? Please open a new issue on our github page, thanks!

--- src/CustomQuery/iZeaoGamer/Main.php
+++ src/CustomQuery/iZeaoGamer/Main.php
@@ -3,6 +3,8 @@ namespace CustomQuery\iZeaoGamer;
 use pocketmine\utils\Config;
 use pocketmine\event\server\QueryRegenerateEvent;
 use pocketmine\event\Listener;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
 use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
 
@@ -44,4 +46,145 @@ $event->setPlayerCount(mt_rand($minPlayerCount, $maxPlayerCount));
         }
      }
    }
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+if($command->getName() === "customquery"){
+    if(!$sender->hasPermission("customquery.settings")){
+        $sender->sendMessage(TextFormat::colorize("&cYou don't have permission to use this command."));
+        return false;
+    }
+    if(!isset($args[0])){
+        $sender->sendMessage(TextFormat::colorize("&aUsage incorrect. Please use: &b/customquery <setting> <value>"));
+        return true;
+    }
 }
+    if($args[0] === "reload"){
+        $this->reloadConfig();
+        $sender->sendMessage(TextFormat::colorize("&aConfig reloaded successfully."));
+        return true;
+    }
+    if($args[0] === "list-plugins"){
+        if(!isset($args[1])){
+            $sender->sendMessage(TextFormat::colorize("&cOption must return a value. true/false."));
+            return true;
+        }
+    }
+        if($args[1] === "true"){
+            $sender->sendMessage(TextFormat::colorize("&dYou've set list-plugins option to true."));
+            $config->set("list-plugins", true);
+        } else {
+            if($args[1] === "false"){
+                $sender->sendMessage(TextFormat::colorize("&dYou've set list-plugins option to false."));
+                $config->set("list-plugins", false);
+                return true;
+        }
+        }
+        if($args[0] === "infinity-slots"){
+            if(!isset($args[1])){
+                $sender->sendMessage(TextFormat::colorize("&cOption must return a value. true/false."));
+                return true;
+            }
+        }
+            if($args[1] === "true"){
+                $sender->sendMessage(TextFormat::colorize("&dYou've set infinity slots option to true."));
+                $config->set("infinity-slots", true);
+            } else {
+                if($args[1] === "false"){
+                $sender->sendMessage(TextFormat::colorize("&dYou've set infinity slots option to false."));
+                $config->set("infinity-slots", false);
+            }
+            }
+            if($args[0] === "enable-serverlist-motd"){
+                if(!isset($args[1])){
+                    $sender->sendMessage(TextFormat::colorize("&cOption must return a value. true/false."));
+                    return true;
+                }
+            }
+                if($args[1] === "true"){
+                    $sender->sendMessage(TextFormat::colorize("&dYou've set enable-serverlist-motd option to true."));
+                    $config->set("enable-serverlist-motd", true);
+                } else {
+                    if($args[1] === "false"){
+                    $sender->sendMessage(TextFormat::colorize("&dYou've set enable-serverlist-motd option to false."));
+                    $config->set("enable-serverlist-motd", false);
+                }
+                }
+                if($args[0] === "fake-slots"){
+                    if(!isset($args[1])){
+                        $sender->sendMessage(TextFormat::colorize("&cOption must return a value. true/false."));
+                        return true;
+                    }
+                }
+                    if($args[1] === "true"){
+                        $sender->sendMessage(TextFormat::colorize("&dYou've set fake-slots option to true."));
+                        $config->set("fake-slots", true);
+                    } else {
+                        if($args[1] === "false"){
+                        $sender->sendMessage(TextFormat::colorize("&dYou've set fake-slots option to false."));
+                        $config->set("fake-slots", false);
+                    }
+                    }
+                    if($args[0] === "default-server-name"){
+                        if(!isset($args[1])){
+                            $sender->sendMessage(TextFormat::colorize("&cOption must return a value. true/false."));
+                            return true;
+                        }
+                    }
+                        if($args[1] === "true"){
+                            $sender->sendMessage(TextFormat::colorize("&dYou've set default-server-name option to true."));
+                            $config->set("default-server-name", true);
+                        } else {
+                            if($args[1] === "false"){
+                            $sender->sendMessage(TextFormat::colorize("&dYou've set default-server-name option to false."));
+                            $config->set("default-server-name", false);
+                        }
+                        }
+                        if($args[0] === "set-plugins"){
+                            if(!isset($args[1])){
+                                $sender->sendMessage(TextFormat::colorize("&cOption cannot be empty. Use: /customquery set-plugins <plugins>"));
+                                return true;
+                            }
+                        }
+                                if(!in_array($sender->getName(), $config->get("set-plugins"))){
+                                    $sender->sendMessage(TextFormat::colorize("&cPlease use arrays []."));
+                                } else {
+                                    $config->set("set-plugins", $args[1]);
+                                        $sender->sendMessage(TextFormat::colorize("&dYou've set 'set-plugins' to &5" . $args[1] . "&d."));
+                                        return true;
+                                    }
+                        if($args[0] === "motd-serverlist-message"){
+                            $config->set("motd-serverlist-message", $args[1]);
+                            $sender->sendMessage(TextFormat::colorize("&dYou've set motd-serverlist-message to&5 " . $args[1] . "&d."));
+                            return true;
+                        }
+                        if($args[0] === "new-player-motd"){
+                            $config->set("new-player-motd", $args[1]);
+                            $sender->sendMessage(TextFormat::colorize("&dYou've set 'new-player-motd' message to " . $args[1]));
+                            return true;
+                        }
+                        if($args[0] === "old-player-motd"){
+                            $config->set("old-player-motd", $args[1]);
+                            $sender->sendMessage(TextFormat::colorize("&dYou've set 'old-player-motd' message to " . $args[1]));
+                            return true;
+                        }
+                        if($args[0] === "min-slots"){
+                            if(!is_int($args[1])){
+                                $sender->sendMessage(TextFormat::colorize("&cInvalid option - Option must be a numeric number, $args[1] given."));
+                            } else {
+                                $config->set("min-slots", $args[1]);
+                                $sender->sendMessage(TextFormat::colorize("&dYou've set 'min-slots' option to &5" . $args[1]));
+                                return true;
+                            }
+                        }
+                            if($args[0] === "max-slots"){
+                                if(!is_int($args[1])){
+                                    $sender->sendMessage(TextFormat::colorize("&cInvalid option - Option must be a numeric number, $args[1] given."));
+                                } else {
+                                    $config->set("max-slots", $args[1]);
+                                    $sender->sendMessage(TextFormat::colorize("&dYou've set 'max-slots' option to &5" . $args[1]));
+                                    return true;
+                                }
+                            }
+                            return true;
+                        }
+                    }
+
