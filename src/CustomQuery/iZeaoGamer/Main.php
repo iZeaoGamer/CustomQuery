7c239b4d7ef782f90c43a93cd2335d89fb3b0393
iZeaoGamer <jameshayward14@icloud.com>
2019-05-15 11:22:46 +0100

    !== v1.1.0-BETA UPDATE !==
    * Moved all config/update related content to \utils\Utils folder.
    * Fixed all dependency problems. Thanks.
    * Renamed some functions that allows the server to check for config and its plugin releases / updates.
    * Added Utils.php file.
    * Added a public construct to Utils. (May make construct for Utils private soon.)
    * Added full support for ConfigUpdater and UpdateNotifier.
    * Bug fixes and improvements were made in relations to Update notifications and config updaters.

--- src/CustomQuery/iZeaoGamer/Main.php
+++ src/CustomQuery/iZeaoGamer/Main.php
@@ -4,9 +4,6 @@ namespace CustomQuery\iZeaoGamer;
 use pocketmine\event\server\QueryRegenerateEvent;
 use pocketmine\event\Listener;
 
-use JackMD\ConfigUpdater\ConfigUpdater;
-use JackMD\UpdateNotifier\UpdateNotifier;
-
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 
@@ -14,17 +11,21 @@ use pocketmine\plugin\PluginBase;
 
 use CustomQuery\iZeaoGamer\tasks\ServerNameTask;
 use CustomQuery\iZeaoGamer\commands\CustomQueryCommand;
+use CustomQuery\iZeaoGamer\utils\Utils;
 
 
 class Main extends PluginBase implements Listener{
+    
     /** @var int */
-	private const CONFIG_VERSION = 0.2;
-	
+    private const CONFIG_VERSION = 0.2;
+    
+    public function onLoad(): void{
+        Utils::UpdateNotifier();
+    }
     public function onEnable(): void{
         if(!is_file($this->getDataFolder() . "config.yml")){
             $this->saveDefaultConfig();
-		 $this->checkConfig();
-            UpdateNotifier::checkUpdate($this, $this->getDescription()->getName(), $this->getDescription()->getVersion());
+         Utils::ConfigUpdater();
         $config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         if (!is_dir($this->getDataFolder())) { @mkdir($this->getDataFolder()); }
@@ -39,13 +40,6 @@ class Main extends PluginBase implements Listener{
       }
      }
     }
-    /**
-	 * Checks if the config is up-to-date.
-	 */
-	private function checkConfig(): void{
-        $config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
-		ConfigUpdater::checkUpdate($this, $config, "config-version", self::CONFIG_VERSION);
-	}
     public function onQuery(QueryRegenerateEvent $event){
 	    $config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
         if($config->get("list-plugins") === true){
---
+++ src/CustomQuery/iZeaoGamer/utils/Utils.php
@@ -0,0 +1,38 @@
+<?php 
+namespace CustomQuery\utils;
+use JackMD\ConfigUpdater\ConfigUpdater;
+use JackMD\UpdateNotifier\UpdateNotifier;
+use pocketmine\utils\Config;
+use CustomQuery\iZeaoGamer\Main;
+use RuntimeException;
+
+class Utils{
+
+    public function construct__(Main $plugin){
+        $this->plugin = $plugin;
+    }
+     /** @var int */
+     private const CONFIG_VERSION = 0.2;
+
+    /**
+	 * Checks if the required virions/libraries are present before enabling the plugin.
+	 */
+	public static function checkVirions(): void{
+		$virionsRequired = [
+			UpdateNotifier::class,
+			ConfigUpdater::class
+		];
+		foreach($virionsRequired as $class){
+			if(!class_exists($class)){
+				throw new RuntimeException("CustomQuery plugin will only work if you use the plugin phar from Poggit.");
+			}
+		}
+    }
+    public static function UpdateNotifer(): void{
+        UpdateNotifier::checkUpdate($this->plugin, $this->plugin->getDescription()->getName(), $this->plugin->getDescription()->getVersion());
+    }
+    private static function ConfigUpdater(): void{
+        $config = new Config($this->plugin->getDataFolder() . "config.yml", Config::YAML, array());
+		ConfigUpdater::checkUpdate($this->plugin, $config, "config-version", self::CONFIG_VERSION);
+    }
+}
